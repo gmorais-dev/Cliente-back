3c4017b13956cc7503694a0fc82057fbe74d2b7a
Guilherme morais <111903592+gmorais-dev@users.noreply.github.com>
2023-08-12 10:06:39 -0300

    camada service

---
+++ src/main/java/com/cliente/aplicacao/service.java
@@ -0,0 +1,18 @@
+@Service
+public class ClienteService { @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente cadastrarCliente(Cliente cliente) {
+        if (clienteRepository.findByCpfCnpj(cliente.getCpfCnpj()) != null) {
+            throw new IllegalArgumentException("Já existe um cliente cadastrado com o CPF/CNPJ informado");
+        }
+        return clienteRepository.save(cliente);
+    }
+
+
+}
+
+
+
+    
+    
