f2ce7544653e1b45ee92fd3dfd297bedaa49bb48
gmorais-dev1~ <guilherme_morais14@hotmail.com>
2023-08-29 12:44:53 -0300

    token de usuario

---
+++ src/main/java/com/cliente/aplicacao/model/JwtToken.java/Cliente.java
@@ -0,0 +1,40 @@
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "usuarios")
+
+
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", allocationSize = 1)
+    private Long id;
+
+
+
+@Column(nullable = false,unique = true)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean emailVerificado = false;
+
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleEnum role;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<JwtToken> jwtTokens;
+     }
+    
---
+++ src/main/java/com/cliente/aplicacao/model/JwtToken.java/Jwt.Token.java
@@ -0,0 +1,27 @@
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+
+
+     public class JwToken{ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "jwt_token_seq")
+    @SequenceGenerator(name = "jwt_token_seq", allocationSize = 1)
+    private Long id;
+  
+
+     @NotNull
+    @Enumerated(EnumType.STRING)
+    private TokenEnum tokenTipo;
+    private String valor;
+    private Long codigoVerificacao;
+ @ManyToOne(targetEntity = Usuario.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "usuario_id")
+    private Usuario usuario;
+     }
